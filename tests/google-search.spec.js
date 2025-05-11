830b13aec6856f98aece7dc2cef59a5fc1aad930
gururajhm <gurrajhm1977@gmail.com>
2025-05-11 17:05:49 +0530

    Add Playwright Live workflow

--- tests/google-search.spec.js
+++ tests/google-search.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');  
+import { test, expect } from '@playwright/test';  
   
 test('Google search test', async ({ page }) => {  
   // Navigate to Google  
