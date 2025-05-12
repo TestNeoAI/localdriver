8394e289a9d5147137b29c8a87d5733a31f35bb1
gururajhm <gurrajhm1977@gmail.com>
2025-05-12 22:01:58 +0530

    docker changes

--- .docker/server.py
+++ .docker/server.py
@@ -2,18 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 import subprocess
 import os
+
 app = Flask(__name__)
-CORS(app)  # Enables CORS for all routes
-
-
+CORS(app)
 
 @app.route('/run-test', methods=['GET'])
 def run_test():
-    import os
-    env = os.environ.copy()
-    env["DISPLAY"] = ":1"
-    subprocess.Popen(["/bin/bash", "/run-tests.sh"], env=env)
-    return "Playwright test started!", 200
+    os.environ["DISPLAY"] = ":1"
+    result = subprocess.run(["/run-tests.sh"], capture_output=True, text=True)
+    return result.stdout + result.stderr
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6081)
