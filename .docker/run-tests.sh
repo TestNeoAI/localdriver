9e3e1e264a221026c38c7eb3d762c2a83880b757
gururajhm <gurrajhm1977@gmail.com>
2025-05-16 12:57:35 +0530

    docker file changes

--- .docker/run-tests.sh
+++ .docker/run-tests.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export DISPLAY=:1
-npx playwright test /tests/google-search.spec.js --headed > /tmp/test.log 2>&1
-cat /tmp/test.log
+npx playwright test /tests/google-search.spec.js > /tmp/test.log 2>&1
